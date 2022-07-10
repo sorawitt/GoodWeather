e096865116626cabe708e2f9d3331433edbc9e87
sorawit <sorawit.tr@gmail.com>
2022-07-10 14:59:55 +0700

    Initial Commit

---
+++ GoodWeather/GoodWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherApp.swift
+//  GoodWeather
+//
+//  Created by Sorawit Trutsat on 10/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
